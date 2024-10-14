5c16e56f7120f128ffc29911ec9d105f112abc70
Noa <ohknoa@gmail.com>
2024-10-14 21:35:54 +0530

    advance indexing

--- day1.py
+++ day1.py
@@ -1,29 +1,36 @@
 import numpy as np 
 
-#broadcasting 
+#advance indexing 
 
-a = np.array([1,2,3])
-b = np.array([5])
+#this is the now one indexing in the numpy 
 
-print(a+b) # in this numpy automatically make 3 element in b of 5 means 1+5,2+5,3+5 that it
 
-#imp note 
-# in the numpy broadcastinng array are only compatiabel when they have same as them or they have one 
-# eg 1st array dimneisonal are [3,2]
-# 2st array dimneisonal can be like this [1,2] or [3,1] or [3,2] or [1,1]
-#above you can see the rule of compatible that we should have a same number or 1 and also 1 is compatible with any numbner 
+a = np.array([[1,2,3],
+              [4,5,6],
+              [7,8,9]])
 
-#example 
+print(a[0:3]) #by this we are selecting the rows like in th eg we are selecting the whole matrix if we do [1:3]w
+#we will select the second row means 1 index row and 3 row a(2 index row )
 
-a1 = np.array([1,2]) #(1,2) this will be 
-a2 = np.array([[2],[3]]) #(2,1)
+#by using the list for the selecting 
 
-#how this work ?
-# 1st array is 1 D and other is 2 D 
-#we can see that in the 2nd array the 0 index element have two elemnts so because of this 1st array we become twice menas like this :-
-# [[1,2],[1,2]]
-# so this complete the process and then 2nd array 1st index have 1 elment only and 1st array have 2 elments so this make the 2nd array like this :-
-# [[2,2],[3,3]]
-#now the addition will be like this:- 1+2,2+2,1+3,2+3 and final is :- [[3,4],[4,5]]
+print(a[[0,2]]) # in this we are selecting only o index row ( 1 row ) and the 2 index row ( means  3 rd row ) 
 
-print(a1 + a2)
+#imp indexing method( dimensional indexing)
+
+print(a[:,:]) # in this first jam(:)  means for the row and second is for columns 
+print(a[0,:]) # this will give only [1,2,3] becuase we selected only o index row 
+print(a[:,0]) # this will give only [1,4,7] becuase we selected only o index columns 
+#and as on we can give 1 index in both rows and columns and 2 index and so on !!!!!
+#we have np.newaxis property for giving the new axises 
+print(a[np.newaxis,0,:]) #u can see the difference in both above one and below one we got addtional axises
+print(a[:,0,np.newaxis]) #same here also  we got it in horizontal 
+
+# we can mix the dimensional indexing iwth other indexing also 
+#eg 
+
+print(a[[0,2],[2,0], np.newaxis]) #in this will get only [3 ,7]
+
+# we use bolleans for the indexing also like this : - 
+
+print(a[[0],[True,False,True]]) # in this we will get the [1 ,3 ] because i gave the false value to the 2 and i only selected the 1st row !!
