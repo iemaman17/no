0ebdcdd41f95cd94895a4575d98ca06fba999326
Noa <ohknoa@gmail.com>
2024-10-14 11:13:49 +0530

    Random Values

--- day1.py
+++ day1.py
@@ -1,3 +1,22 @@
 import numpy as np 
 
-a = np.random.randint(50,size=((5,10)))
+#creating random array with numpy
+
+# This is eg of rand() this generator only positive number
+a = np.random.rand(5)
+print(a) # this will create a random array between the value of 0 and 1 and this array will of 5 values in it and its a 1 d array 
+
+b = np.random.rand(3,5)
+print(b) # this is the 2 d array !!!
+
+#randn() this genrator number close to 0 but they can be negative and positive also 
+c = np.random.randn(2,5)
+print(c)
+
+#ranf() this will generate the random float values 
+d = np.random.ranf((2,4))
+print(d)
+
+#randint for the integer values 
+d1 = np.random.randint(0,100,(2,10))
+print(d1)
