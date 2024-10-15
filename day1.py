c4e2b858b2542c423e4e4c625a4052f110975e11
Noa <ohknoa@gmail.com>
2024-10-15 14:56:56 +0530

    searching and iterating

--- day1.py
+++ day1.py
@@ -1,19 +1,32 @@
 import numpy as np
-#sorting the array 
-a = np.array([[3,8,2],
-              [6,4,1],
-              [7,0,9]])
 
-print(np.sort(np.sort(a,axis=0),axis=1)) #this method is good but its not dinemical purely 
+#searching and iterating over the array 
 
-#another method
+#searching 
 
-b = a.copy()
-print(b)
+a = np.array([2,4,3,8,0.1,38,5,1,0,9,6,88,76,0.1])
 
-b=b.flatten()
-b=np.sort(b)
-print(b.reshape(a.shape)) # by using this we can make sorting more dynamic 
+print(np.argmax(a)) # this give the index of max value 
+print(a.max()) # this give the max value 
+
+print(np.argmin(a)) # min value index 
+print(a.min()) #min value 
+
+print(np.nonzero(a)) # give the index of value who are non zero  
+
+# iteraing 
+
+a1 = np.arange(9).reshape(3,3)
+print(a1)
+
+for items in np.nditer(a1,order="C"):
+    print(items, end=" ")#this is iteraing vertically 
+print()
+
+for items in np.nditer(a1,order="F"):
+    print(items, end=" ")
+
+
 
 
 
